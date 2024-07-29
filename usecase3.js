fbf7aa52732a54dece44ee5041ece1e9ee008034
patibandasripriya <107873041+patibandasripriya@users.noreply.github.com>
2024-07-29 19:26:16 +0530

    Create usecase3.js

---
+++ usecase3.js
@@ -0,0 +1,11 @@
+const work_hours=8;
+let employee = "Abhishek";
+
+function PartTimeEmpWage(){
+    let worked_days=4;
+    return worked_days*work_hours*20;
+}
+
+let EmpWage=PartTimeEmpWage();
+
+console.log("Part time employee",employee,"has total wage of : ",EmpWage);
